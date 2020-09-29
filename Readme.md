17a36838935013c154e97b6fd2ca07227e4b1007
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-09-29 16:55:49 +0200

    INITIAL COMMIT

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# PotenCalc
+
+Use:
+
+- Get it with `npm i protenzcalcsh`
+- then add it to your app with the line `const prot = require("protenzcalcsh");`
+- If you want now to calc `2³` and print it out you add the line `console.log(prot(2,3).toString());`
+
+`require("protenzcalcsh")(base, exponent)`
+
+&copy;Sharkbyteprojects
---
+++ potenz.ts
@@ -0,0 +1,25 @@
+function proten(basis: number, exponent: number): number {
+  let numbertom: number = 0;
+  if (exponent != 0) {
+    let multi: boolean = true;
+    let first: boolean = true;
+    let exponents: number = exponent;
+    if (exponent < 0) {
+      multi = false;
+      exponents = exponent * -1;
+    }
+    for (let numb = 0; numb < exponents; numb++) {
+      if (first) {
+        numbertom = basis;
+        first = false;
+      } else {
+        numbertom *= basis;
+      }
+    }
+    if (!multi) {
+      numbertom = 1 / numbertom;
+    }
+  }
+  return numbertom;
+}
+module.exports = proten;

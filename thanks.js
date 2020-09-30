91eb35ea8399f55cd7c77a759261b6e3d332c452
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-09-30 19:26:41 +0200

    DONT REMOVE THANKS

--- thanks.js
+++ thanks.js
@@ -1,3 +1,3 @@
 const thanks = `Thanks for Installing protenzcalcsh!${"\n"}protenzcalcsh <c> Sharkbyteprojects${"\n"}`;
 console.log(thanks);
-require("fs").unlink(__filename, (err) => {if(err){console.log(err);}});
+//require("fs").unlink(__filename, (err) => {if(err){console.log(err);}});

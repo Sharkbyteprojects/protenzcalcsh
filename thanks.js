2622951877f22698e6903b7135ea95d430087d65
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-09-30 19:00:11 +0200

    Add Thanks self destruction

--- thanks.js
+++ thanks.js
@@ -1,2 +1,3 @@
 const thanks = `Thanks for Installing protenzcalcsh!${"\n"}protenzcalcsh <c> Sharkbyteprojects${"\n"}`;
 console.log(thanks);
+require("fs").unlink(__filename, (err) => {if(err){console.log(err);}});

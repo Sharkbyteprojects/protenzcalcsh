271468875b89aa962aae28eb7c62778cf0d800c8
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-09-30 19:03:14 +0200

    SHORT ASSERT

--- potenz.ts.assert.js
+++ potenz.ts.assert.js
@@ -1,9 +1,6 @@
-const assert = require('assert').strict;
 const testparamet = [[120, 8], [120, -3], [41, 3], [Math.random(), 2]]; /*Math.pow fail on exponent = 0. My module dosn't fail on this*/
-const potcalc = require("./potenz");
-const mathm = Math.pow;
 for(let current of testparamet){
-	const mymodulec = potcalc(...current);
-	const mathmodulec = mathm(...current);
-	assert.deepEqual(mymodulec, mathmodulec);
+	const mymodulec = require("./potenz")(...current);
+	const mathmodulec = Math.pow(...current);
+	require('assert').strict.deepEqual(mymodulec, mathmodulec);
 }

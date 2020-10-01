e19edf30b68a114a8defd3f1626f69a8d23dea8e
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2020-10-01 14:04:50 +0200

    update

--- potenz.ts
+++ potenz.ts
@@ -5,24 +5,30 @@ Better Than Math.pow
 */
 function proten(basis: number, exponent: number): number {
   let numbertom: number = 0;
-  if (exponent != 0) {
-    let multi: boolean = true;
-    let first: boolean = true;
-    let exponents: number = exponent;
-    if (exponent < 0) {
-      multi = false;
-      exponents = exponent * -1;
-    }
-    for (let numb = 0; numb < exponents; numb++) {
-      if (first) {
-        numbertom = basis;
-        first = false;
-      } else {
-        numbertom *= basis;
+  if (exponent == Infinity) {
+    numbertom = Infinity;
+  } else {
+    if (exponent != 0) {
+      let multi: boolean = true;
+      let first: boolean = true;
+      let exponents: number = exponent;
+      if (exponent < 0) {
+        multi = false;
+        exponents = exponent * -1;
       }
-    }
-    if (!multi) {
-      numbertom = 1 / numbertom;
+      for (let numb = 0; numb < exponents; numb++) {
+        if (first) {
+          numbertom = basis;
+          first = false;
+        } else {
+          numbertom *= basis;
+        }
+      }
+      if (!multi) {
+        numbertom = 1 / numbertom;
+      }
+    } else {
+      numbertom = 1;
     }
   }
   return numbertom;
--- potenz.ts.assert.js
+++ potenz.ts.assert.js
@@ -1,6 +1,12 @@
-const testparamet = [[120, 8], [120, -3], [41, 3], [Math.random(), 2]]; /*Math.pow fail on exponent = 0. My module dosn't fail on this*/
-for(let current of testparamet){
-	const mymodulec = require("./potenz")(...current);
-	const mathmodulec = Math.pow(...current);
-	require('assert').strict.deepEqual(mymodulec, mathmodulec);
+const testparamet = [
+  [120, 8],
+  [120, -3],
+  [41, 3],
+  [Math.random(), 2],
+  [2, 0],
+];
+for (let current of testparamet) {
+  const mymodulec = require("./potenz")(...current);
+  const mathmodulec = Math.pow(...current);
+  require("assert").strict.deepEqual(mymodulec, mathmodulec);
 }
